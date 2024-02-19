44fe6b158eef01c6a1f4c260811638bd58b09f29
Joao Miraya <joaomiraya01@gmail.com>
2024-02-19 13:31:24 -0300

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuizzComponent } from "./components/quizz/quizz.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, QuizzComponent]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'BuzzFeed';
 }
